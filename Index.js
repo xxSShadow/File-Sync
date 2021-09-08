f59d57c83c6daa4afb65ae9e9111cbf0dd1147e3
Brian Leyva <55462869+xxSShadow@users.noreply.github.com>
2021-09-08 10:06:04 -0700

    Update Index.js

--- Index.js
+++ Index.js
@@ -7,3 +7,18 @@ for (File of Folder) {
   Command = require(`./Commands/${File}`);  
   console.log(`Name: ${Command.name}`);
 }
+
+==================================================================================
+
+FileSync = require('fs');
+
+FileSync.readdir('./Commands/', (err, files) => {
+  if (err) {console.error(err)}
+  
+  files.forEach(file => {
+    if (!file.endsWith('.js')) {return}    
+    let Command = require(`./Commands/${file}`);
+    let Name = file.split('.')[0];
+    console.log(`File: ${Name}.js`);
+  });
+});
